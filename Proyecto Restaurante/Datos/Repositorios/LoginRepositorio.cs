c51783307598e88c22d5a485b75b690475f73a91
MrGato01 <marconyenamorado@gmail.com>
2023-04-21 13:36:58 -0600

    plantilla inicial
    creación del proyecto

---
+++ Proyecto Restaurante/Datos/Repositorios/LoginRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using Datos.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.Repositorios
+{
+    public class LoginRepositorio : ILoginRepositorio
+    {
+        public Task<bool> ValidarUsuario(Login login)
+        {
+
+        }
+    }
+}
