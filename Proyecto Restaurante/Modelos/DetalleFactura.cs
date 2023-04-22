d546e2ef7b3571eadac3988727ab9b7ad3440492
yohanna <yohannavilchez02@gmail.com>
2023-04-22 21:52:38 +0000

    Factura / Detalles
    Atributos de Factura y Detalles de la Factura

---
+++ Proyecto Restaurante/Modelos/DetalleFactura.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    public class DetalleFactura
+    {
+        public int NumFactura { get; set; }
+        public int  CodMenu { get; set; }
+        public string Descripcion { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Precio { get; set; }
+        public decimal SubTotal { get; set; }
+        public decimal Descuento { get; set; }
+        public decimal ISV { get; set; }
+        public decimal Total { get; set; }
+    }
+}
---
+++ Proyecto Restaurante/Modelos/Factura.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    public class Factura
+    {
+        public DateTime Fecha { get; set; }
+        public int NumFactura { get; set; }
+        public string Cliente { get; set; }
+        public string IdCliente { get; set; }
+        public decimal  SubTotal { get; set; }
+        public decimal Descuento { get; set; }
+        public decimal ISV { get; set; }
+    }
+}
