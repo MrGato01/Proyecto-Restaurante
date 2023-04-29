bbf540fc343bf796f850b711b88c45ba10f9eb87
emilcemidence <emmc0798@gmail.com>
2023-04-28 20:30:36 -0600

    Productos1

---
+++ Proyecto Restaurante/Datos/Interfaces/IProductosRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.Interfaces
+{
+    public interface IProductosRepositorio
+    {
+        Task<bool> Nuevo(Menu Menu);
+        Task<bool> Actualizar(Menu Menu);
+        Task<bool> Eliminar(string CodigoMenu);
+        Task<IEnumerable<Menu>> GetLista();
+        Task<Menu> GetPorCodigo(string CodigoMenu);
+    }
+}
