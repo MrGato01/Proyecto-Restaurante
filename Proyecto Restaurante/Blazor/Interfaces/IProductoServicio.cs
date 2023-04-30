75dba57ecb57a39405c19bac78ce11b6ffbeee81
MARTIN DANIEL CHANG AGUILERA <martin.chang@unah.hn>
2023-04-29 23:44:27 -0600

    Productos
    Adicion de Productos

---
+++ Proyecto Restaurante/Blazor/Interfaces/IProductoServicio.cs
@@ -0,0 +1,13 @@
+﻿using Modelos;
+
+namespace Blazor.Interfaces
+{
+    interface IProductoServicio
+    {
+        Task<bool> Nuevo(Producto producto);
+        Task<bool> Actualizar(Producto producto);
+        Task<bool> Eliminar(string codigo);
+        Task<IEnumerable<Producto>> GetLista();
+        Task<Producto> GetPorCodigo(string codigo);
+    }
+}
